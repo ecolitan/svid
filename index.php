e656ab028382424f485f9ded0a0f5f9090e8ff50
Aaron Cossey <aaron@cyan.usenet.net.nz>
2015-03-10 01:15:45 +0000

    first working version

--- index.php
+++ index.php
@@ -1,29 +1,66 @@
 <html>
 <head>
   <title>Svid</title>
+  <meta http-equiv="refresh" content="30">
 </head>
 <body>
 
 <table style="width:100%" border="1">
+<tr>
+  <th>Filename</th>
+  <th>Size (bytes)</th>
+  <th>Status</th>
+  <th>Encode</th>
+</tr>
+
 <?php
-function beginEncoding($filename) {
-  echo("/home/transmission/" . $filename);
+
+function beginEncoding($fstring) {
+  $farray = explode(',', $fstring);
+  $jobfile = fopen("../queue/" . $farray[1] . ".job", "w");
+  fwrite($jobfile, $farray[0]);
+  fclose($jobfile);
 }
 
 if (isset($_POST['encode'])) {
   beginEncoding($_POST['encode']);
- // fork encoding process off and write logs to file
 }
 
-$all_files = explode("\n", shell_exec('find /home/transmission -type f -printf "%f\n"'));
+//path,filename,size(bytes)
+$all_files = explode("\n", shell_exec('find /home/transmission -type f -printf "%h,%f,%s\n"'));
 foreach ($all_files as $fstring) {
+  if ($fstring != '') {
+    $farray = explode(',', $fstring);
+  } else {
+    continue;
+  }
+
   echo("<tr>\n");
-  echo("  <td>" . $fstring . "</td>\n");
+
+  // Filename
+  echo("  <td>" . $farray[1] . "</td>\n");
+
+  // Size
+  echo("  <td>" . $farray[2] . "</td>\n");
+
+  // Status
+  if (file_exists("../queue/" . $farray[1] . ".job")) {
+    echo("  <td>Queued</td>");
+  } elseif (file_exists("../processing/" . $farray[1] . ".job")) {
+    echo("  <td>Processing</td>");
+  } elseif (file_exists("../video/" . $farray[1] . ".html")) {
+    echo("  <td><a href='../video/" . $farray[1] . ".html'>video</a></td>");
+  } else {
+    echo("  <td></td>");
+  }
+
+  // Encode Button
   echo("  <td>\n");
   echo('    <form action="index.php" method="post">');
   echo('      <button type="submit" value="' . $fstring . '" name="encode">Encode</button>' . "\n");
   echo('    </form>');
   echo("  </td>\n");
+
   echo("</tr>\n");
 }
 ?>
---
+++ processqueue.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import shutil
+import time
+import datetime
+
+basedir = "/var/www/streamer.ecolitan.net.nz"
+queuedir = os.path.join(basedir, "queue")
+processingdir = os.path.join(basedir, "processing")
+videodir = os.path.join(basedir, "video")
+logdir = os.path.join(basedir, "log")
+
+def log(job, msg):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    logfile = os.path.join(logdir, job)
+    with open(logfile, "a") as f:
+        f.write(st + " " + msg + "\n")
+
+queue = os.listdir(queuedir)
+try:
+    job = queue[0]
+except IndexError:
+    sys.exit(0)
+
+logfile = os.path.join(logdir, job)
+
+# Move job file into in-progress state
+log(job,"starting {}".format(job))
+shutil.move(os.path.join(queuedir, job), os.path.join(processingdir, job))
+
+# Determine Video filename
+filename = job[:-4]
+
+# Determine full input video path
+with open(os.path.join(processingdir, job)) as f:
+    input_vidpath = os.path.join(f.read(),  filename)
+
+# Determine output video path
+output_vidpath = os.path.join(videodir, filename + ".mp4")
+
+# Encode file and write output to logfile
+encode_command = ['/usr/bin/avconv', '-i', input_vidpath , '-c:v', 'libx264', '-strict', 'experimental', output_vidpath]
+log(job, str(encode_command))
+encodeprocess = subprocess.Popen(encode_command, stderr=open(logfile, "a"), stdout=open(logfile, "a"))
+returncode = encodeprocess.wait()
+os.remove(os.path.join(processingdir, job))
+
+if returncode != 0:
+    log(job, "an error occured processing the video")
+    sys.exit(1)
+
+# Write html file
+html_filepath = os.path.join(videodir, filename + ".html")
+openhtml = """<html>
+<head>
+  <title>{}</title>
+</head>
+<body>
+  <video id="{}" src="{}" controls>mp4</video>
+  <br><br>
+</body>
+</html>
+""".format(filename, filename, filename + ".mp4")
+
+with open(html_filepath, "w") as f:
+    f.write(openhtml)
