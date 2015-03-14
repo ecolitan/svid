11d10af36ce186b671ed3a8f7ec2c00290aefe45
Aaron Cossey <aaron@cyan.usenet.net.nz>
2015-03-14 05:16:05 +0000

    add seperate table for encoded videos

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <head>
   <title>Svid</title>
   <meta http-equiv="refresh" content="30">
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
 
-<table style="width:100%" border="1">
+<table style="width:100%">
 <tr>
   <th>Filename</th>
   <th>Size (bytes)</th>
@@ -22,8 +23,19 @@ function beginEncoding($fstring) {
   fclose($jobfile);
 }
 
+function deleteVideo($fstring) {
+  $farray = explode(',', $fstring);
+  $htmlfilename = "../video/" . $farray[1];
+  $vidfilename = "../video/" . substr($farray[1], 0, -5) . ".mp4";
+  unlink($htmlfilename);
+  unlink($vidfilename);
+}
+
 if (isset($_POST['encode'])) {
   beginEncoding($_POST['encode']);
+}
+if (isset($_POST['delete'])) {
+  deleteVideo($_POST['delete']);
 }
 
 //path,filename,size(bytes)
@@ -64,5 +76,41 @@ foreach ($all_files as $fstring) {
   echo("</tr>\n");
 }
 ?>
+</table>
+
+<table style="width:100%">
+<br><br><br><br>
+<tr>
+  <th>Encoded File</th>
+  <th>Link</th>
+  <th>Delete</th>
+</tr>
+
+<?php
+$all_files = explode("\n", shell_exec('find ../video -name "*.html" -type f -printf "%h,%f\n"'));
+foreach ($all_files as $fstring) {
+  if ($fstring != '') {
+    $farray = explode(',', $fstring);
+  } else {
+    continue;
+  }
+
+  echo("<tr>\n");
+  // Filename
+  echo("  <td>" . substr($farray[1], 0, -5) . "</td>\n");
+  // Link
+  echo("  <td><a href='../video/" . $farray[1] . "'>Link</a></td>\n");
+  // Delete Button
+  echo("  <td>\n");
+  echo('    <form action="index.php" method="post">');
+  echo('      <button type="submit" value="' . $fstring . '" name="delete">Delete</button>' . "\n");
+  echo('    </form>');
+  echo("  </td>\n");
+
+  echo("</tr>\n");
+}
+
+?>
+</table>
 </body>
 </html>
